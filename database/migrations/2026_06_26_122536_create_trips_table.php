3f53dc6ced53127a5e58ac39f6408d6eef497833
Umayantha <umayantha@globaltechdevelop.com>
2026-06-26 18:11:52 +0530

    implement migrations

---
+++ database/migrations/2026_06_26_122536_create_trips_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('schedule_id')->constrained()->onDelete('cascade');
+            $table->date('departure_date');
+            $table->enum('status', ['scheduled', 'delayed', 'completed', 'cancelled'])->default('scheduled');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trips');
+    }
+};
